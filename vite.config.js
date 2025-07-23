788763099478917dbeabe922de707e035cc1698f
Fernando Malaspina <f_malaspina@hotmail.com>
2025-07-23 08:18:49 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  //base: '/vallebot/',
-
+  base: './',
+  build: {
+    outDir: 'docs', // Esto hace que el build se coloque en /docs
+  }
 })
 
 
